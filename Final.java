f2580b5322298a23b28b85efecb9bca8236e5d53
MatiasR85 <matute.22@live.com.ar>
2025-08-19 12:01:11 -0600

    Semana 1

---
+++ Final.java
@@ -0,0 +1,117 @@
+
+package pkgfinal;
+
+public class Final {
+
+    
+    public static void main(String[] args) {
+     
+        System.out.println("Hola Java!");
+        
+  /* 
+     Ejercicio3 
+        String Nombre = "Matias";  
+           
+   int Edad = 22;
+   
+   double Altura = 1.68;
+   
+   boolean Estudiante = true;
+   
+        System.out.println(Nombre);
+        System.out.println(Edad);
+        System.out.println(Altura);
+        System.out.println(Estudiante);
+        */    
+  
+  /*
+  Ejercicio4
+   public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+  
+        String Nombre;
+        int Edad;
+     
+        System.out.print("Ingrese Su Nombre: ");
+        Nombre = input.nextLine();
+     
+        System.out.print("Ingrese su edad: ");
+        Edad = Integer.parseInt(input.nextLine());
+     
+        
+    System.out.println(" Mi nombre es " +Nombre + ";" + " Tengo " +Edad + " Anios " );
+        
+  */
+  
+  /*
+  Ejercicio5
+  
+    public static void main(String[] args) {
+      Scanner input = new Scanner(System.in);
+      
+    int N1, N2, Suma, Resta, Division, Multiplicacion;
+        System.out.print("Ingrese el primer numero entero: ");
+        N1 = Integer.parseInt(input.nextLine());
+      
+        System.out.print("Ingrese el segundo numero entero: ");
+        N2 = Integer.parseInt(input.nextLine());
+        
+        Suma = N1 + N2;
+        System.out.println(" La suma es: " +Suma);
+        
+        Resta = N1 - N2;
+        System.out.println(" La Resta es: "  +Resta);
+        
+        Multiplicacion = N1 * N2;
+        System.out.println(" La Multiplicacion es: "  +Multiplicacion);
+        
+        Division = N1 / N2;
+        System.out.println(" La Division es: " +Division);
+  */
+  
+  /*
+  Ejercicio6 
+  
+  public static void main(String[] args) {
+        
+    String Nombre = " Juan Perez ";
+    String Domicilio = "Calle Falsa 123";
+    int Edad = 30;
+    
+        System.out.println("\n" + Nombre +"\n" +Domicilio +"\n" +Edad);
+    
+  */
+  
+  /*
+  Ejercicio8 
+  
+  public static void main(String[] args) {
+    Scanner input = new Scanner(System.in);
+    
+    int N1, N2;
+    double Division;
+    
+    System.out.print("Ingrese el primer numero: ");
+    N1 = Integer.parseInt(input.nextLine());
+        
+    System.out.print("Ingrese el segundo numero: ");
+    N2 = Integer.parseInt(input.nextLine());
+        
+    Division = ((double) N1 / N2); 
+        System.out.println(Division);
+    
+  */
+  
+  /*
+  Ejercicio9 
+    public static void main(String[] args)
+      Scanner scanner = new Scanner(System.in); 
+    
+   String Nombre;
+    System.out.print("Ingresa tu nombre: "); 
+    
+    Nombre = scanner.nextLine();
+        System.out.println("Hola, " + Nombre);
+  */
+ }
+}
