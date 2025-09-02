121f14c9905fc7c86373081c3b104810a9f25d7a
MatiasR85 <matute.22@live.com.ar>
2025-09-02 09:44:35 -0600

    SEMANA 3 CODIGO MAIN

---
+++ TP3_FINAL.java
@@ -0,0 +1,156 @@
+
+package javaapplication39;
+
+
+public class JavaApplication39 {
+
+    
+    public static void main(String[] args) {
+        System.out.println("TP 3 ");
+        
+    /* EJERCICIO 1 
+        
+package ej1;
+
+import java.util.Scanner;
+
+
+public class EJ1 {
+
+    
+    public static void main(String[] args) {
+      
+     Estudiantes e = new Estudiantes();   
+        
+     e.Nombre = " Matias ";
+     e.Apellido = " Roda  ";
+     e.Curso = " 2a ";
+     e.Nota = 8 ;
+     
+     e.mostrarinfo();
+     
+     e.subirCalificacion(2);
+     
+     e.Nota = 6;
+     
+     
+   
+    }
+    
+}
+
+        
+        
+        */
+    
+    
+/*EJERCICO2
+    package ej2;
+
+import java.util.Scanner;
+
+
+public class EJ2 {
+
+    
+    public static void main(String[] args) {
+ 
+    Mascotaa m = new Mascotaa();    
+    
+    
+    m.Nombre = " Azul ";
+    m.Especie = "Gato ";
+    m.Edad = 9 ;
+    
+    m.mostrarDatos();
+ 
+    m.cumplirAnios(2);
+    }
+    
+}
+    */    
+
+/* EJERCICIO 3
+
+package ej3;
+
+import java.util.Scanner;
+
+
+public class EJ3 {
+
+    
+    public static void main(String[] args) {
+    Libro l = new Libro();
+    
+        System.out.println("Autor " + l.getAutor());
+        System.out.println("Titulo " + l.getTitulo());
+
+        
+       l.setAnoPublicacion(1999);
+       l.setAnoPublicacion(-10);
+    }
+    
+}
+
+
+*/
+
+/*EJERCICIO 4 
+
+package ej4;
+
+import java.util.Scanner;
+
+
+public class EJ4 {
+
+    
+    public static void main(String[] args) {
+     Gallina g1  = new Gallina(1,2);
+     Gallina g2  = new Gallina(2,2);
+     
+     g1.ponerHuevo();
+     g1.ponerHuevo();
+     g1.envejecer();
+     
+     g2.ponerHuevo();
+     g2.ponerHuevo();
+     g2.envejecer();
+     
+     g1.mostrarEstados();
+     g2.mostrarEstados();
+     
+    }
+    
+}
+
+
+*/
+
+/*EJERCICIO 5 
+
+package ej5;
+
+public class EJ5 {
+
+    
+    public static void main(String[] args) {
+    Nave nave1 = new Nave("River", 52);
+    
+    nave1.avanzar(60);
+    
+    nave1.recargarCombustible(30);
+    
+    nave1.despegar();
+    nave1.avanzar(30);
+    
+    nave1.mostrarDatos();
+    }
+    
+}
+
+*/
+    }
+    
+}
